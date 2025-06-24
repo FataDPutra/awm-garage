9c4953089bc78b66c81b57dd00eb6b04c8eca6be
Fata Dwi Putra <dwifata28@gmail.com>
2025-06-24 17:29:43 +0700

    fix url offerpricecontroller

--- app/Http/Controllers/OfferPriceController.php
+++ app/Http/Controllers/OfferPriceController.php
@@ -22,7 +22,7 @@ class OfferPriceController extends Controller
 
         $userkey = env('ZENZIVA_USERKEY');
         $passkey = env('ZENZIVA_PASSKEY');
-        $message = "Halo {$user->full_name}, permintaan pembelian Anda (ID: {$purchaseRequest->id}) telah diperbarui: {$statusMessage} Silahkan pantau pesanan anda secara berkala melalui website http://awmgarage.com";
+        $message = "Halo {$user->full_name}, permintaan pembelian Anda (ID: {$purchaseRequest->id}) telah diperbarui: {$statusMessage} Silahkan pantau pesanan anda secara berkala melalui website https://awmgarage.store";
         $url = 'https://console.zenziva.net/wareguler/api/sendWA/';
 
         $response = Http::asForm()->post($url, [
